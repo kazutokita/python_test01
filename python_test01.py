d59c3525fb466cc700c991f261b82f7f0e32a8ca
KAZU-WIN8\kazu <kazu.tokita.net@gmail.com>
2018-06-18 01:53:51 +0900

    Taskは開始タグのみへ変更 Multiのスペルミス修正

--- python_test01.py
+++ python_test01.py
@@ -5,11 +5,15 @@ stack = []
 
 class Handler(xml.sax.handler.ContentHandler):
 	def startElement(self, name, attrs):
-		stack.append(attrs["id"])
-		print("Start: " + name + " / id: " + attrs["id"] + "_START")
+		if name in ["Block", "Multi", "Thread"]:
+			stack.append(attrs["id"])
+			print("Start: " + name + " / id: " + attrs["id"] + "_START")
+		elif name in ["Task"]:
+			print("Start: " + name + " / id: " + attrs["id"])
 
 	def endElement(self, name):
-		print("End: " + name + " / id: " + stack.pop() + "_END")
+		if name in ["Block", "Multi", "Thread"]:
+			print("End: " + name + " / id: " + stack.pop() + "_END")
 	
 	# def characters(self, content):
 	# 	print("character:" + content)
